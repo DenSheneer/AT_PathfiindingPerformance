81a03e8da0e0b9e1a38cc1522db8d6a525aa68ac
Denise <mail.deniseg@gmail.com>
2025-01-21 22:57:26 +0100

    Added dungeon size to PathfindResult

--- Assets/Scripts/PathfindResult.cs
+++ Assets/Scripts/PathfindResult.cs
@@ -2,6 +2,7 @@ public class PathfindResult
 {
     int _dungeonSizeX;
     int _dungeonSizeY;
+    int _nrOfRooms;
     EPathFindMode _pathfindMode;
     double _timeMiliseconds;
     int _solutionSize;
@@ -9,15 +10,17 @@ public class PathfindResult
 
     public int DungeonSizeX { get { return _dungeonSizeX; } }
     public int DungeonSizeY { get { return _dungeonSizeY; } }
+    public int NrOfRooms { get { return _nrOfRooms; } }
     public string PathfindMode { get { return _pathfindMode.ToString(); } }
     public double TimeMilliseconds { get { return _timeMiliseconds; } }
     public int Steps { get { return _solutionSize; } }
     public int RandomSeed { get { return _randomSeed; } }
 
-    public PathfindResult(int dungeonSizeX, int dungeonSizeY, EPathFindMode pathfindMode, double timeMiliseconds, int solutionSize, int randomSeed)
+    public PathfindResult(int dungeonSizeX, int dungeonSizeY, int nrOfRooms, EPathFindMode pathfindMode, double timeMiliseconds, int solutionSize, int randomSeed)
     {
         _dungeonSizeX = dungeonSizeX;
         _dungeonSizeY = dungeonSizeY;
+        _nrOfRooms = nrOfRooms;
         _pathfindMode = pathfindMode;
         _timeMiliseconds = timeMiliseconds;
         _solutionSize = solutionSize;
